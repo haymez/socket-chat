e0f481144d67c3c00c1ad4d48807d9d5a95be98e
James Miltenberger <jamilt13@gmail.com>
2015-06-25 19:53:48 -0500

    Got initial chat working

--- src/js/router.js
+++ src/js/router.js
@@ -3,7 +3,30 @@ require('angular');
 var App = angular.module('SocketChat', []);
 
 var fn = function($scope) {
-  $scope.input = '';
+  $scope.chatInput = ''
+  $scope.hasUserName = false;
+  $scope.userName = '';
+
+  var chatServer = new WebSocket("ws://localhost:3000");
+  chatServer.onopen = function(event) {
+  }
+  chatServer.onmessage = function (event) {
+    $scope.messages = JSON.parse(event.data);
+    $scope.$apply();
+  }
+
+  $scope.setUserName = function() {
+    $scope.hasUserName = true
+  }
+
+  $scope.sendChat = function() {
+    var chatObj = {
+      text: $scope.chatInput,
+      userName: $scope.userName,
+    }
+    chatServer.send(JSON.stringify(chatObj));
+    $scope.chatInput = '';
+  }
 }
 
-App.controller('test', ['$scope', fn]);
+App.controller('ChatController', ['$scope', fn]);
